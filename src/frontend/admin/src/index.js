b576b24e531233623c162407f87ba354c82f1224
Feruz Iskandarov <feruz@monitorapp.com>
2021-04-12 17:58:08 +0900

    Add admin login screen

--- src/frontend/admin/src/index.js
+++ src/frontend/admin/src/index.js
@@ -3,26 +3,43 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import Products from './components/Products'
 import ProductDetailedScreen from './screens/ProductDetailedScreen'
-import { Nav, Navbar } from 'react-bootstrap'
 
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
-import 'bootstrap/dist/css/bootstrap.min.css';
 import CreateProductScreen from './screens/ProductCreateScreen'
-
+import LoginScreen from './screens/LoginScreen'
 
 export default class App extends React.Component {
 
     render() {
         return (
             <Router>
-                <Navbar bg="dark" variant="dark">
-                    <Navbar.Brand href="/">Lookum</Navbar.Brand>
-                    <Nav>
-                        <Nav.Link href="/createProduct">Create Product</Nav.Link>
-                        <Nav.Link href="/">Products</Nav.Link>
-                    </Nav>
-                </Navbar>
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+                    <div className="container">
+                        <a className="navbar-brand" href="/">Lookum</a>
+                        
+                        <div className="collapse navbar-collapse" id="navbarResponsive">
+                            <ul className="navbar-nav ml-auto">
+                                <li className="nav-item active">
+                                    <a className="nav-link" href="/">Home</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/createProduct">Create product</a>
+                                </li>
+                            </ul>
+
+                        </div>
+                        <ul className="navbar-nav ml-auto justify-content-end">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="/login">Login</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/register">Register</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+
                 <Switch>
 
                     <Route exact path="/">
@@ -30,6 +47,7 @@ export default class App extends React.Component {
                     </Route>
                     <Route path="/createProduct" children={<CreateProductScreen />} />
                     <Route path="/product/:id" children={<ProductDetailedScreen />} />
+                    <Route path="/login" children={<LoginScreen />} />
                     <Route path="/">
                         <Products />
                     </Route>
